5f32b3a7b3b0e9d89b759e6c34df7cbc917b4f4e
mementohaeri <mementohaeri@gmail.com>
2021-02-01 15:49:28 +0900

    added a news end-point

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const port = process.env.PORT || 3000;
 
 //http://127.0.0.1:3000/
 app.get('/', (req,res) => res.send('Hello, NodeJS!'));
+app.get('/welcome', (req,res)=> res.send('Hi, there. Welcome to the Nodejs service.'));
 //3000번 포트로 nodejs 구동시켜준다.
 app.listen(port, ()=> console.log('Example app listening on port 3000'))
